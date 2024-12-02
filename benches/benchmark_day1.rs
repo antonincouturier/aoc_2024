1a5ffe8c869317253327a61bc615450f2b606cef
Antonin Couturier <acouturier.inscriptions@gmail.com>
2024-12-02 09:16:35 -0500

    Remove comment in benchmark day1

--- benches/benchmark_day1.rs
+++ benches/benchmark_day1.rs
@@ -3,7 +3,6 @@ use aoc_2024::days::day01::{read_input, sorted_difference, similarity_score};
 
 fn benchmark_day01(c: &mut Criterion) {
     let (first, second) = read_input("data/day01.txt");
-    // println!("Input sizes: {} and {}", first.len(), second.len()); 
 
     c.bench_function("sorted_difference", |b| {
         b.iter(|| sorted_difference(black_box(&first), black_box(&second)))
